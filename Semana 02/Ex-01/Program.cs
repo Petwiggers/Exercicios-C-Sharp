41740436e763f13937cefd280d398a327457024c
Peterson Wiggers <petersonwiggersoficial@gmail.com>
2023-04-19 21:54:23 -0300

    Semana 02 Ex-1

---
+++ Semana 02/Ex-01/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+  private static void Main(string[] args)
+  {
+    System.Console.WriteLine(SomarValores(2, 3));
+  }
+  private static double SomarValores(int numero1, int numero2)
+  {
+    return numero1 + numero2;
+  }
+}
