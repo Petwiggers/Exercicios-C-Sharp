8c3083c4262c21a4b7acc00012526e4a49eede9c
Peterson Wiggers <petersonwiggersoficial@gmail.com>
2023-04-20 11:58:18 -0300

    Exercicio 02

--- Semana 02/Ex-01/Program.cs
+++ Semana 02/Ex-01/Program.cs
@@ -2,10 +2,20 @@
 {
   private static void Main(string[] args)
   {
-    System.Console.WriteLine(SomarValores(2, 3));
+    System.Console.WriteLine("Soma de dois vaçores Int: " + SomarValores(2, 3));
+    System.Console.WriteLine("Soma de dois vaçores Double: " + SomarValores(10.0, 12.5));
+    System.Console.WriteLine("Soma de três vaçores Double: " + SomarValores(10.0, 12.5, 20.22));
   }
-  private static double SomarValores(int numero1, int numero2)
+  private static int SomarValores(int numero1, int numero2)
   {
     return numero1 + numero2;
+  }
+  private static double SomarValores(double numero1, double numero2)
+  {
+    return numero1 + numero2;
+  }
+  private static double SomarValores(double numero1, double numero2, double numero3)
+  {
+    return numero1 + numero2 + numero3;
   }
 }
