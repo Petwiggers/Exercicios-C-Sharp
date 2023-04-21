3e564e615d84518f50f6575a52d32d3ce4678ee0
Peterson Wiggers <petersonwiggersoficial@gmail.com>
2023-04-20 21:55:38 -0300

    Exercicio 05

---
+++ Semana 02/Exercicios-03-ao-10/Carro.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Exercicios_03_ao_10
+{
+  public class Carro
+  {
+    public string Marca;
+    public string Modelo;
+    public string Placa;
+    public string Cor;
+    public Proprietario Proprietario;
+  }
+}
