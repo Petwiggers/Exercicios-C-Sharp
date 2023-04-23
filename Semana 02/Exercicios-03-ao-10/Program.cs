f093bc36e07e3501e757b6e956d6a789238f284e
Peterson Wiggers <petersonwiggersoficial@gmail.com>
2023-04-23 18:17:12 -0300

    Exercicio 09

--- Semana 02/Exercicios-03-ao-10/Program.cs
+++ Semana 02/Exercicios-03-ao-10/Program.cs
@@ -17,35 +17,48 @@ internal class Program
     carros.Add(carro2);
     carros.Add(carro3);
 
-    CadastrarCarro();
 
-    void CadastrarCarro()
+    ListarCarros();
+    // void CadastrarCarro()
+    // {
+
+    //   Carro carro = new Carro();
+    //   System.Console.WriteLine("Digite as informações do carro!");
+    //   System.Console.Write("\nMarca: ");
+    //   carro.Marca = Console.ReadLine();
+    //   System.Console.Write("Modelo: ");
+    //   carro.Modelo = Console.ReadLine();
+    //   System.Console.WriteLine("Ano:");
+    //   carro.Ano = Console.ReadLine();
+    //   System.Console.WriteLine("Placa:");
+    //   carro.Placa = Console.ReadLine();
+    //   System.Console.WriteLine("Cor: ");
+    //   carro.Cor = Console.ReadLine();
+
+    //   Proprietario proprietario = new Proprietario();
+    //   System.Console.WriteLine("Digite as informações do proprietário !! ");
+    //   System.Console.Write("\nNome : ");
+    //   proprietario.Nome = Console.ReadLine();
+    //   System.Console.Write("Cpf: ");
+    //   proprietario.Cpf = Console.ReadLine();
+    //   System.Console.Write("Telefone: ");
+    //   proprietario.Telefone = Console.ReadLine();
+
+    //   carro.Proprietario = proprietario;
+    //   carros.Add(carro);
+    // }
+
+    void ListarCarros()
     {
-
-      Carro carro = new Carro();
-      System.Console.WriteLine("Digite as informações do carro!");
-      System.Console.Write("\nMarca: ");
-      carro.Marca = Console.ReadLine();
-      System.Console.Write("Modelo: ");
-      carro.Modelo = Console.ReadLine();
-      System.Console.WriteLine("Ano:");
-      carro.Ano = Console.ReadLine();
-      System.Console.WriteLine("Placa:");
-      carro.Placa = Console.ReadLine();
-      System.Console.WriteLine("Cor: ");
-      carro.Cor = Console.ReadLine();
-
-      Proprietario proprietario = new Proprietario();
-      System.Console.WriteLine("Digite as informações do proprietário !! ");
-      System.Console.Write("\nNome : ");
-      proprietario.Nome = Console.ReadLine();
-      System.Console.Write("Cpf: ");
-      proprietario.Cpf = Console.ReadLine();
-      System.Console.Write("Telefone: ");
-      proprietario.Telefone = Console.ReadLine();
-
-      carro.Proprietario = proprietario;
-      carros.Add(carro);
+      foreach (var carro in carros)
+      {
+        System.Console.WriteLine($"\n\nCarro do Proprietário : {carro.Proprietario.Nome}");
+        System.Console.WriteLine($"\nMarca : {carro.Marca}");
+        System.Console.WriteLine($"Modelo : {carro.Modelo}");
+        System.Console.WriteLine($"Ano de fabricaçao : {carro.Ano}");
+        System.Console.WriteLine($"Placa do Carro : {carro.Placa}");
+        System.Console.WriteLine($"Cor do Carro : {carro.Cor}");
+      }
     }
   }
 }
