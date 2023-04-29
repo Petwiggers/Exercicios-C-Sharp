5b1e9392fd7f369c1797ce99e2fadb08f9be4df4
Peterson Wiggers <petersonwiggersoficial@gmail.com>
2023-04-28 21:32:55 -0300

    S03 Ex-06

---
+++ Semana 03/Ex-06/Program.cs
@@ -0,0 +1,6 @@
+﻿using Ex_06;
+
+Retangulo retangulo = new Retangulo();
+retangulo.DefinirAltura(100);
+retangulo.DefinirLargura(500);
+retangulo.GetRetangulo();
---
+++ Semana 03/Ex-06/Retangulo.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ex_06
+{
+  public class Retangulo
+  {
+    private double _largura;
+    private double _altura;
+
+    public double Largura
+    {
+      get { return _largura; }
+      private set { _largura = value; }
+    }
+
+    public double Altura
+    {
+      get { return _altura; }
+      private set { _altura = value; }
+    }
+
+    public void DefinirAltura(double altura)
+    {
+      if (altura > 0)
+      {
+        _altura = altura;
+      }
+      else
+      {
+        System.Console.WriteLine("A altura deve ser um valor positivo !");
+      }
+    }
+
+    public void DefinirLargura(double largura)
+    {
+      if (largura > 0)
+      {
+        _largura = largura;
+      }
+      else
+      {
+        System.Console.WriteLine("A largura deve ser um valor positivo !");
+      }
+    }
+    public void GetRetangulo()
+    {
+      System.Console.WriteLine($"O Retangulo possui: \n\nAltura: {_altura}\nLargura: {_largura}");
+    }
+  }
+}
