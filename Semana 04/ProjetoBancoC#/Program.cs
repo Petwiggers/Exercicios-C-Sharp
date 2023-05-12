08b98a74a298be19c74e9f6b8522db0877dca816
Peterson Wiggers <petersonwiggersoficial@gmail.com>
2023-05-12 11:56:40 -0300

    [M2S05] Ex 10 - Crie um menu de aplicação para o projeto do banco

--- Semana 04/ProjetoBancoC#/Program.cs
+++ Semana 04/ProjetoBancoC#/Program.cs
@@ -2,12 +2,56 @@
 using ProjetoBancoC_.Serviços;
 
 
-ClienteService clienteService = new ClienteService();
-clienteService.ComandoInicial();
-clienteService.CriarConta();
-clienteService.ExibirClientes();
-Cliente cliente = clienteService.BuscarClientePorNumeroDeConta(1);
-System.Console.WriteLine("Cliente da conta 1 = " + cliente.ResumoCliente());
+ExibirMenu();
+
+void ExibirMenu()
+{
+  string opcao;
+  do
+  {
+    System.Console.WriteLine("1 - Abrir uma nova Conta");
+    System.Console.WriteLine("2 - Consultar Conta");
+    System.Console.WriteLine("3 - Listar Contas");
+    System.Console.WriteLine("4 - Sair");
+    System.Console.WriteLine();
+    System.Console.Write("Digite a opção desejada: ");
+    opcao = Console.ReadLine();
+    ServicoSelecionado(opcao);
+  } while (opcao != "4");
+}
+
+void ServicoSelecionado(string opcao)
+{
+  ClienteService clienteService = new ClienteService();
+  switch (opcao)
+  {
+    case "1":
+      clienteService.CriarConta();
+      break;
+    case "2":
+      System.Console.WriteLine("Digite o numero da conta desejada: ");
+      int conta = int.Parse(Console.ReadLine());
+      var cliente = clienteService.BuscarClientePorNumeroDeConta(conta);
+      if (cliente == null)
+      {
+        System.Console.WriteLine("Conta não Cadastrada:");
+      }
+      else
+      {
+        System.Console.WriteLine(cliente.ResumoCliente());
+      }
+      break;
+    case "3":
+      clienteService.ExibirClientes();
+      break;
+    case "4":
+      System.Console.WriteLine("Você saiu do Programa !");
+      break;
+    default:
+      System.Console.WriteLine("Opção Inválida");
+      break;
+  }
+}
 
 
 
