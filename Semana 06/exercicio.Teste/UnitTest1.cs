f1eb48f22876e682cc1aa28b9753349371713f2b
Peterson Wiggers <petersonwiggersoficial@gmail.com>
2023-05-17 11:44:10 -0300

    [M2S06] Ex 2 - Preparando os testes

---
+++ Semana 06/exercicio.Teste/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace exercicio.Teste;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
