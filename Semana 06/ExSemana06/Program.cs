9f3b04145b50b104cd4a27ac1f4c8356aafcc5ae
Peterson Wiggers <petersonwiggersoficial@gmail.com>
2023-05-18 21:32:12 -0300

    [M2S06] Ex 6 - Praticando lógica de programação (Parte 2)

--- Semana 06/ExSemana06/Program.cs
+++ Semana 06/ExSemana06/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ExSemana06;
+
+
+List<int> valores = new List<int>() { 8, 4, 9, 6 };
+List<int> listaConvertida = InvertendoValores.Inverter(valores);
+
+foreach (var item in listaConvertida)
+{
+  System.Console.Write(item + ",");
+}
